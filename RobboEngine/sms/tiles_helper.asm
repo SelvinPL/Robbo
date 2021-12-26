cecef5140eb884b8c4fc75f537d3153f06d8b079
Przemek Sulikowski <P.Sulikowski@osadkowski.pl>
2021-12-26 22:17:02 +0100

    platform specific asm

---
+++ RobboEngine/sms/tiles_helper.asm
@@ -0,0 +1,4 @@
+		.title  "TILES HELPERS ASM"
+		.module tiles_helpers_asm
+		.area _CODE
+		.globl			_map_to_tiles_hi
---
+++ RobboEngine/tiles_helper_main.asm
@@ -0,0 +1,5 @@
+		.title  "TILES HELPERS ASM"
+		.module tiles_helpers_asm
+		.area _CODE
+		.globl			_map_to_tiles_hi
+		.include "tiles_helper.asm"
